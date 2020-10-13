2461d07002f3b2b51df92a30c18592ba1f9bb6ff
gerardo ponce <poncegerardo.p@gmail.com>
2020-10-13 17:20:57 -0500

    model User v2

--- com.tecsup.practica_1/src/main/java/com/tecsup/lab01/model/User.java
+++ com.tecsup.practica_1/src/main/java/com/tecsup/lab01/model/User.java
@@ -7,5 +7,36 @@ public class User {
 	private String nombres;
 	private String apellido_paterno;
 	private int edad;
+	
+	public User(String nombres, String apellido_paterno, int edad) {
+		super();
+		this.nombres = nombres;
+		this.apellido_paterno = apellido_paterno;
+		this.edad = edad;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getApellido_paterno() {
+		return apellido_paterno;
+	}
+
+	public void setApellido_paterno(String apellido_paterno) {
+		this.apellido_paterno = apellido_paterno;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
 
 }
