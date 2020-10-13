e516b000dc032bee579236a49f040630624e6faf
Alvaro Fabian <alvarofabian0432@gmail.com>
2020-10-13 16:19:16 -0500

    proyecto base

---
+++ com.tecsup.practica_1/src/main/java/com/tecsup/lab01/com/tecsup/practica_1/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab01.com.tecsup.practica_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
